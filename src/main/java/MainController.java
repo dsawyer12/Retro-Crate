99c18afa01a497024acc80e8effabc233e71da59
dsawyer12 <d_sawyer12@yahoo.com>
2020-03-04 20:01:41 -0600

    updated from MAC

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,3 +1,4 @@
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -9,9 +10,11 @@ import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -24,7 +27,7 @@ public class MainController implements Initializable, EventHandler<MouseEvent> {
     TextField searchBar;
 
     @FXML
-    Label topPaneTitle;
+    Label topPaneTitle, consoleDescription;
 
     @FXML
     private ListView<Console> listView;
@@ -33,25 +36,29 @@ public class MainController implements Initializable, EventHandler<MouseEvent> {
     private Map<Console, Image> imgCollection;
 
     public MainController() throws IOException {
-        ArrayList<String> columns = new ArrayList<>();
-        columns.add("id integer PRIMARY KEY");
-        columns.add("name text NOT NULL");
-        columns.add("capacity real");
-        DatabaseManager.createNewTable("new table", columns);
-//        projectObservableList = FXCollections.observableArrayList();
-//        projectObservableList.addAll(ContentScraper.getConsoleData());
-//        imgCollection = new HashMap<>();
-//        for (Console console : projectObservableList) {
-//            if (console.getImgPath() != null)
-//                imgCollection.put(console, new Image(new FileInputStream(console.getImgPath())));
-//        }
+//        ArrayList<String> columns = new ArrayList<>();
+//        columns.add("id integer PRIMARY KEY");
+//        columns.add("name text NOT NULL");
+//        columns.add("capacity real");
+//        DatabaseManager.createNewTable("new table", columns);
+        projectObservableList = FXCollections.observableArrayList();
+        projectObservableList.addAll(ContentScraper.getConsoleData());
+        imgCollection = new HashMap<>();
+        for (Console console : projectObservableList) {
+            if (console.getImgPath() != null)
+                imgCollection.put(console, new Image(new FileInputStream(console.getImgPath())));
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         listView.setCellFactory(projectListView -> new DirectoryListItem(imgCollection));
         listView.setItems(projectObservableList);
-        listView.setOnMouseClicked(event -> topPaneTitle.setText(listView.getSelectionModel().getSelectedItem().getName()));
+        listView.setOnMouseClicked(event -> {
+            topPaneTitle.setText(listView.getSelectionModel().getSelectedItem().getName());
+            consoleDescription.setText(listView.getSelectionModel().getSelectedItem().getDescription());
+        }
+        );
         DragResizer.makeResizable(sideNav, 5);
         DragResizer.makeResizable(listView, 0);
     }
